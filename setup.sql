2939b76ba693ffc4ef5cac073e24ad3e482b4d79
angelrene45 <ar.herrera0@gmail.com>
2025-03-26 23:56:07 -0400

    Update file

--- setup.sql
+++ setup.sql
@@ -1,5 +1,5 @@
 - [Store the private and public keys securely](https://docs.snowflake.com/en/user-guide/key-pair-auth)
-```sql
+
 ALTER USER ANGELCORTEX2025 SET RSA_PUBLIC_KEY='MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAz8KtWSZfTYUXB9CLuwy/
 MnbfRoNi7Xv3okWX2ozXh5ag5qZj8VwtAxtMmKV1fQgkdVmcQ1zQ76QG8iYxU4vz
 Fe0Q0bNhEnfUEpx0DCfa8EFUebWWrO60SQMIWmdCKuulbInBMeU5Kq8jk3pCRuZQ
@@ -7,7 +7,3 @@ H2KOSoGbqQfv6tI3DUHoe/xXy8qogR95e32nDKM18aYirSZuGpaQl8LOX47sCeK5
 mnHzBKgtickRmCPYi2CYa79RRQzSMNo2sR/5QsnLEDGTlDbiAFEqLg+bgqQm8bTI
 7weDTXdzSqXE6Rp+F+8G2hnGIuVU9eBtEEFDOdJKBRvksZ5M+xCb3E6HpQ4Tunul
 FQIDAQAB';
-```
-
-
-ALTER USER ANGELCORTEX2025 set DISABLE_MFA = TRUE;
